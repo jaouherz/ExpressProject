4ed535d10f6f13d01749bc118ac7193bdb5bfd01
jaouher2002 <jaouher2002@gmail.com>
2025-02-24 14:07:10 +0100

    2/25/2025

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
 const mongoose= require('mongoose');
+const userRouter = require('./routes/users');
 require('dotenv').config();
 mongoose.connect(process.env.MONGO_URI).then(()=>{
   console.log('MongoDB Connected!');
 }).catch(err=>{console.log( err)});
-var app = express();
+var app = express()
+
+
+app.use(express.json());
+app.use("user", userRouter)
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {console.log('Server running on port ' + port);})
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const User = require("../models/user");
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post("/", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save;
+    res.status(201).send({ message: "User saved successfully", user });
+    res.status(201).send({ message: "User saved successfully", user });
+  } catch (error) {
+    res.status(400);
+  }
 });
-
 module.exports = router;
