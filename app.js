62ed53ca845a51c652af01c5cef04086109486d7
jaouher2002 <jaouher2002@gmail.com>
2025-02-26 14:50:53 +0100

    2/25/2025

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express()
 
 
 app.use(express.json());
-app.use("user", userRouter)
+app.use("/user", userRouter)
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {console.log('Server running on port ' + port);})
